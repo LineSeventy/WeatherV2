e94155a0da119578c4e773f32a7d1859e5b7b207
LineSeventy <169619934+LineSeventy@users.noreply.github.com.>
2026-02-18 08:19:22 +0800

    At least it is working

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Header from '../Components/Header'
+import { error } from 'console';
 
-
+interface weatherData {
+  location: {
+    name: string;
+    region: string;
+    country: string;
+  };
+  current: {
+    temp_c: number;
+    temp_f: number;
+    condition: {
+      text: string;
+      icon: string;
+    };
+  };
+}
 export default function Home() {
   
   const [location, setLocation] = useState<string>("")
+  const [weatherInfo, setWeatheInfo]= useState<string>("")
   const [userInput,setUserInput] = useState("")
+  const weatherApiKey:string = " ";
+  
+  useEffect(() => {
+     if (!location) return;
+    const fetchWeather= async() =>{
+      try {
+        const response = await fetch(
+          `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${location}&aqi=no`);
+          if(!response.ok){
+            throw new Error("Failed to Fetch")
+          }
+          const data: weatherData = await response.json();
+          setWeatheInfo(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchWeather();
+  }, [location])
   
   const handleLocation = (name:string) =>{
     setLocation(name)
+    
   }
   return (
     <>
@@ -24,7 +60,11 @@ export default function Home() {
       </label>
     </form>
     <button onClick={()=> handleLocation(userInput)} >Search</button>
-    <p>{location}  </p>
+    {weatherInfo && (
+      <p>
+        {weatherInfo.location.name} - {weatherInfo.current.condition.text}
+      </p>
+    )}
     </>
   )
 }
